430abc960f3f9f83c35d265b5677025bf69b44e8
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-10-29 11:44:24 -0400

    Added script to delete x64

---
+++ deletex64.ps1
@@ -0,0 +1,29 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$targetFolder
+)
+
+# Function to remove x64 folders within a given directory path
+function Remove-X64FoldersInDirectory {
+    param ([string]$path)
+    
+    Get-ChildItem -Path $path -Recurse -Force -Directory -Filter "x64" | ForEach-Object {
+        try {
+            $_ | Get-ChildItem -Recurse -Force | ForEach-Object { $_.Attributes = 'Normal' }
+            Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction Stop
+            Write-Output "Deleted: $($_.FullName)"
+        }
+        catch {
+            Write-Output "Failed to delete: $($_.FullName) - $_"
+        }
+    }
+}
+
+# Check if the specified folder exists
+if (Test-Path -Path $targetFolder) {
+    # Delete all x64 folders in the directory and subdirectories
+    Remove-X64FoldersInDirectory -path $targetFolder
+    Write-Output "All x64 folders have been deleted from $targetFolder"
+} else {
+    Write-Output "The specified folder path does not exist: $targetFolder"
+}
